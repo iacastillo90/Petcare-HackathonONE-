339b799f50486c02fd44fa5691c501010dc12c66
Ivan Castillo <137194587+iacastillo90@users.noreply.github.com>
2025-08-18 21:47:24 -0400

    first commit

---
+++ src/test/java/com/Petcare/Petcare/PetcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Petcare.Petcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
