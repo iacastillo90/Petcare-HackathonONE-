8b97e5b6a0ce5ddda9ecb976a4c7c6c0660fa6fd
luifer991 <luifer991@gmail.com>
2025-08-24 15:36:07 -0500

    refactor for using tests

--- src/test/java/com/Petcare/Petcare/PetcareApplicationTests.java
+++ src/test/java/com/Petcare/Petcare/PetcareApplicationTests.java
@@ -7,9 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 		"spring.datasource.url=jdbc:h2:mem:testdb",
 		"spring.datasource.driver-class-name=org.h2.Driver",
 		"spring.jpa.hibernate.ddl-auto=create-drop",
-		"jwt.secret=testSecret123456789012345678901234567890",
-		"spring.mail.username=test@test.com",
-		"spring.mail.password=test"
+		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration"
 })
 class PetcareApplicationTests {
 
