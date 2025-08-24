37f56c0412aa355138c0b563de7c34d1160fdb07
luifer991 <luifer991@gmail.com>
2025-08-24 14:32:38 -0500

    refactor for using tests

--- src/test/java/com/Petcare/Petcare/PetcareApplicationTests.java
+++ src/test/java/com/Petcare/Petcare/PetcareApplicationTests.java
@@ -3,7 +3,14 @@ package com.Petcare.Petcare;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.datasource.url=jdbc:h2:mem:testdb",
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.jpa.hibernate.ddl-auto=create-drop",
+		"jwt.secret=testSecret123456789012345678901234567890",
+		"spring.mail.username=test@test.com",
+		"spring.mail.password=test"
+})
 class PetcareApplicationTests {
 
 	@Test
