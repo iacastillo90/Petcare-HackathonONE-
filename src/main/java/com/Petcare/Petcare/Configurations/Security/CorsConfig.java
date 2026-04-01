2c6f7210528df9b4b63cfd61b9435c9053bfb9bf
Ivan Castillo <137194587+iacastillo90@users.noreply.github.com>
2026-04-01 01:36:33 -0300

    Cambio de ruta front en cors

--- src/main/java/com/Petcare/Petcare/Configurations/Security/CorsConfig.java
+++ src/main/java/com/Petcare/Petcare/Configurations/Security/CorsConfig.java
@@ -23,7 +23,7 @@ public class CorsConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         // 1. Orígenes permitidos (El frontend exacto)
-        configuration.setAllowedOrigins(List.of("https://frontend-pet-403q5exqo-iacastillo90s-projects.vercel.app","http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("https://frontend-pet-dwjhqaciw-iacastillo90s-projects.vercel.app/","http://localhost:5173"));
 
         // 2. Métodos HTTP permitidos
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
